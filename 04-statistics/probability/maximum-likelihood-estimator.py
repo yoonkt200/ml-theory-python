02dee008118313432b79f5573c5225ceeffe1190
yoonkt200 <yoonkt200@gmail.com>
2018-08-05 00:37:22 +0900

    MLE complete

--- 04-statistics/probability/maximum-likelihood-estimator.py
+++ 04-statistics/probability/maximum-likelihood-estimator.py
@@ -18,22 +18,85 @@ class MLE():
         self._verbose = verbose
 
 
-    def pdf(self, x):
+    def likelihood(self, x, M):
         """
-        Probability Density Function
+        Probability Density Function is Normal distribution
+        PDF's y is same as likelihood
+
         :param x:
-        :return: likelihood of input x (same as y of pdf)
+        :return: likelihood of input x (likelihood of input x is same as y of pdf)
         """
-        return (1 / math.sqrt(2*math.pi) * math.pow(self._std, 2)) * math.exp(-(pow(x-self._m, 2) / (2*math.pow(self._std, 2))))
+        return (1 / math.sqrt(2*math.pi) * math.pow(self._std, 2)) * np.exp(-(np.power(x - M, 2) / (2*math.pow(self._std, 2))))
 
 
     def fit(self):
-        pass
+        """
+        training estimator
+        M, which minimizes Likelihood, is obtained by the gradient descent method.
+        M is the MLE of the samples
+        """
+
+        # init M
+        self._estimator = np.random.normal(self._m, self._std, 1)
+
+        # train while epochs
+        self._training_process = []
+        for epoch in range(self._epochs):
+            likelihood = np.prod(self.likelihood(self._samples, self._m))
+            prediction = np.prod(self.likelihood(self._samples, self._estimator))
+            cost = self.cost(likelihood, prediction)
+            self._training_process.append((epoch, cost))
+            self.update(self._samples, self._estimator)
+
+            # print status
+            if self._verbose == True and ((epoch + 1) % 10 == 0):
+                print("Iteration: %d ; cost = %.4f" % (epoch + 1, cost))
 
 
-    def cost(self):
-        pass
+    def cost(self, likelihood, prediction):
+        """
+        cost function
+        :param likelihood: likelihood of population
+        :param prediction: likelihood in samples
+        :return: the cost of optimizing the parameters
+        """
+        return math.sqrt(likelihood - prediction)
 
 
-    def update(self):
-        pass
+    def update(self, x, M):
+        """
+        update in gradient descent
+        gradient is approximated
+        :param x: samples
+        :param M: estimator
+        """
+        gradient = np.sum(np.exp(-(np.power(x - M, 2) / (2*math.pow(self._std, 2)))))
+        if self._m > self._estimator:
+            self._estimator += self._learning_rate * gradient
+        else:
+            self._estimator -= self._learning_rate * gradient
+
+
+    def get_mle(self):
+        """
+        parameter getter
+        :return: estimator of MLE
+        """
+        return self._estimator
+
+
+# run example
+if __name__ == "__main__":
+
+    # samples for MLE
+    samples = np.array([64, 64.5, 65, 65.5, 66])
+
+    # assumptions about the population
+    mean = np.array([65.0])
+    std = 5
+
+    # get MLE
+    estimator = MLE(samples, mean, std, learning_rate=0.1, epochs=30, verbose=True)
+    estimator.fit()
+    result = estimator.get_mle()
+    print(result)
